c746d588058e375cefb0cc0c9ace37ad82500954
LewysW <lewysward99@gmail.com>
2018-09-22 14:58:32 +0100

    Implemented own strlen and strcpy and got file name from command line argument

--- myls.c
+++ myls.c
@@ -5,16 +5,38 @@
 #include <time.h>
 #include <dirent.h>
 
+#include<stdio.h> //TODO - REMOVE THIS IMPORT STATEMENT AND ALL STDLIB FUNCTIONS
+
 // A complete list of linux system call numbers can be found in: /usr/include/asm/unistd_64.h
 #define WRITE_SYSCALL 1
 
+int mystat();
+int mywire();
+int myopen();
+int myStrLen(char* str);
+void myStrCpy(char* dest, const char* src, size_t n);
+
 int main(int argc, char** argv)
 {
-    char* text = "Hello world!\n";    // String to print
-    size_t len = 14;     // Length of our string, which we need to pass to write syscall
-    long handle = 1;     // 1 for stdout, 2 for stderr, file handle from open() for files
-    long ret = -1;       // Return value received from the system call
+    //If file specified, get the file name
+    if (argc == 2) {
+        int size = myStrLen(argv[1]);
+        char fileName[size];
+        myStrCpy(fileName, argv[1], size);
+    }
+    return 0;
+}
 
-    ret = write(handle, text, len);
-    return ret;
+int myStrLen(char* str) {
+    int i;
+    for (i = 0; str[i] != '\0'; i++);
+    return i;
+}
+
+void myStrCpy(char* dest, const char* src, size_t n) {
+    size_t i;
+    for (i = 0; i < n; i++) {
+        dest[i] = src[i];
+    }
+    dest[i] = '\0';
 }
